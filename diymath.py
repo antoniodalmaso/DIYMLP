62a8fb4c29fe65aaa0a87582159687d5d5781b30
Antonio Dal Maso <antonio.dalmaso.2000@gmail.com>
2025-06-01 22:20:39 +0200

    basic Matrix class

--- diymath.py
+++ diymath.py
@@ -1,6 +1,6 @@
 class Vector:
     def __init__(this, N):
-        this.__values = [0]*N
+        this.__values = [0 for i in range(N)]
         this.__N = N
 
     def get(this, idx):
@@ -60,6 +60,41 @@ class Vector:
 
         for idx in range(this.size()):
             res.set(idx, f(this.get(idx)))
+
+        return res
+    
+    def print(this):
+        print(this.__values)
+
+
+
+class Matrix:
+    def __init__(this, N,M):
+        this.__values = [[0 for j in range(M)] for i in range(N)]
+        this.__N = N
+        this.__M = M
+
+    def get(this, i, j):
+        return this.__values[i][j]
+    
+    def set(this, i, j, n):
+        this.__values[i][j] = n
+
+    def size(this):
+        return this.__N, this.__M
+    
+    def dotVector(this, v):
+        N, M = this.size()
+
+        if M != v.size():
+            raise Exception("Dot product is not possible if the number of columns of the matrix is different from the size of the vector")
+        
+        res = Vector(N)
+        for i in range(N):
+            aux = 0
+            for j in range(M):
+                aux += this.get(i,j)*v.get(j)
+            res.set(i, aux)
 
         return res
     
